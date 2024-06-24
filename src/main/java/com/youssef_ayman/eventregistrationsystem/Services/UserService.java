18ccac3bf36b0e68b6f28e36d702a8dfd4a4e8ee
yosp313 <147242847+yosp313@users.noreply.github.com>
2024-06-24 22:57:36 +0300

    removed security and tested the event api

--- src/main/java/com/youssef_ayman/eventregistrationsystem/Services/UserService.java
+++ src/main/java/com/youssef_ayman/eventregistrationsystem/Services/UserService.java
@@ -3,9 +3,6 @@ package com.youssef_ayman.eventregistrationsystem.Services;
 import com.youssef_ayman.eventregistrationsystem.Models.User;
 import com.youssef_ayman.eventregistrationsystem.Repositories.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
